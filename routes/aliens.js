606deeeff27876f876969ad517e07001c5d4b8f1
Ramhardik88 <58719444+Ramhardik88@users.noreply.github.com>
2020-05-14 06:58:30 +0530

    initial commit in curdjs

---
+++ routes/aliens.js
@@ -0,0 +1,75 @@
+const express = require('express')
+
+const router= express.Router()
+
+const Alien=require('../models/alien')
+
+
+router.get('/', async(req,res)=>{
+    try{
+        const aliens =await Alien.find()
+        res.json(aliens)
+
+
+    }catch(err)
+    {
+        res.send('error'+err)
+    }
+})
+
+router.get('/:id', async(req,res)=>{
+    try{
+        const alien =await Alien.findById(req.params.id)
+        res.json(alien)
+
+
+    }catch(err)
+    {
+        res.send('error'+err)
+    }
+})
+
+router.post('/',async(req,res)=>{
+    const alien=new Alien({
+        name:req.body.name,
+        tech:req.body.tech,
+        sub:req.body.sub
+    })
+
+    try{
+        const a1=await alien.save()
+        res.json(a1)
+
+    }catch(err){
+        res.send('Error in post')
+
+    }
+
+})
+
+router.patch('/:id',async(req,res)=>{
+    try{
+        const alien=await Alien.findById(req.params.id)
+
+        alien.sub=req.body.sub
+        const a1=await alien.save()
+        res.json(a1)    
+
+    }catch(err)
+    {
+        res.send('errer')
+    }
+})
+
+router.delete('/:id',async(req,res)=>{
+    try{
+        const alien= await Alien.findByIdAndDelete(req.params.id)
+        
+        res.json(alien)
+
+    }catch(err){
+        res.send('error cant delete')
+    }
+})
+
+module.exports=router
